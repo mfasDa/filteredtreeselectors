6b81cdffb01d18401556d9fff1b5662742ff3b84
mfasel <mfasel@lbl.gov>
2015-01-30 18:29:35 +0100

    Add include for aliphysics

--- steer/runAnalysisLocal.C
+++ steer/runAnalysisLocal.C
@@ -1,6 +1,7 @@
 void runAnalysisLocal(const char *filelist, const char *selectorname){
   gROOT->Macro("LoadLibs.C");
   gROOT->LoadMacro("MakeChain.C");
+  gSystem->AddIncludePath(Form("-I%s/include",gSystem->Getenv("ALICE_PHYSICS")));
   TChain *particleChain = MakeChain(filelist, "ParticleTree/JetEvent");
   particleChain->Process(Form("%s++", selectorname),"", 123456789);
 }
