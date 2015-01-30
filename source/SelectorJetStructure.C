2a56391919ce064edc3cb2c3e7004e52d9028b81
mfasel <mfasel@lbl.gov>
2015-01-30 19:00:17 +0100

    Add task doing: - number of contibutors in area - sum of pt in area for charged and all constituents with pt above x

---
+++ source/SelectorJetStructure.C
@@ -0,0 +1,210 @@
+#define SelectorJetStructure_cxx
+// The class definition in SelectorJetStructure.h has been generated automatically
+// by the ROOT utility TTree::MakeSelector(). This class is derived
+// from the ROOT class TSelector. For more information on the TSelector
+// framework see $ROOTSYS/README/README.SELECTOR or the ROOT User Manual.
+
+// The following methods are defined in this file:
+//    Begin():        called every time a loop on the tree starts,
+//                    a convenient place to create your histograms.
+//    SlaveBegin():   called after Begin(), when on PROOF called only on the
+//                    slave servers.
+//    Process():      called for each event, in this function you decide what
+//                    to read and fill your histograms.
+//    SlaveTerminate: called at the end of the loop on the tree, when on PROOF
+//                    called only on the slave servers.
+//    Terminate():    called at the end of the loop on the tree,
+//                    a convenient place to draw/fit your histograms.
+//
+// To use this file, try the following session on your Tree T:
+//
+// Root > T->Process("SelectorJetStructure.C")
+// Root > T->Process("SelectorJetStructure.C","some options")
+// Root > T->Process("SelectorJetStructure.C+")
+//
+
+#include "SelectorJetStructure.h"
+#include <TArrayD.h>
+#include <TH2.h>
+#include <THnSparse.h>
+#include <TParticlePDG.h>
+#include <TStyle.h>
+#include <TLorentzVector.h>
+
+#include "AliReducedJetInfo.h"
+#include "AliReducedJetConstituent.h"
+#include "AliReducedJetParticle.h"
+
+using namespace HighPtTracks;
+
+
+void SelectorJetStructure::Begin(TTree * /*tree*/)
+{
+   // The Begin() function is called at the start of the query.
+   // When running with PROOF Begin() is only called on the client.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+
+   TString option = GetOption();
+
+}
+
+void SelectorJetStructure::SlaveBegin(TTree * /*tree*/)
+{
+   // The SlaveBegin() function is called after the Begin() function.
+   // When running with PROOF SlaveBegin() is called on each slave server.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+
+   TString option = GetOption();
+
+   fContribHistoMapAll = new HistoList;
+   fContribHistoMapCharged = new HistoList;
+   fPtHistoMapAll = new HistoList;
+   fPtHistoMapCharged = new HistoList;
+
+   double pttrackmin[] = {0., 0.5, 1., 2., 5., 10., 20., 50.};
+   double rlim[] = {0, 0.05, 0.1, 0.2, 0.3, 0.4};
+
+   TH2 *result = NULL;
+   for(double *ptiter = pttrackmin; ptiter < pttrackmin + sizeof(pttrackmin)/sizeof(double); ++ptiter){
+     for(int irad = 0; irad < 5; irad++){
+       result = new TH2D(Form("hContribAllPt%03dRadmin%03dRadmax%03d", int((*ptiter) *10.), int(rlim[irad]*100), int(rlim[irad+1]*100)),
+           Form("All Particles with pt > %1f GeV/c within radii %.2f and %.2f", *ptiter,rlim[irad],rlim[irad+1]), 100, 0., 500., 101, -0.5 , 100.5);
+       fContribHistoMapAll->Insert(*ptiter, rlim[irad], rlim[irad+1], result);
+       fOutput->Add(result);
+
+       result = new TH2D(Form("hContribChargedPt%03dRadmin%03dRadmax%03d", int((*ptiter) *10.), int(rlim[irad]*100), int(rlim[irad+1]*100)),
+           Form("Charged Particles with pt > %1f GeV/c within radii %.2f and %.2f", *ptiter,rlim[irad],rlim[irad+1]), 100, 0., 500., 101, -0.5 , 100.5);
+       fContribHistoMapCharged->Insert(*ptiter, rlim[irad], rlim[irad+1], result);
+       fOutput->Add(result);
+
+       result = new TH2D(Form("hSumPtAllPt%03dRadmin%03dRadmax%03d", int((*ptiter) *10.), int(rlim[irad]*100), int(rlim[irad+1]*100)),
+           Form("Sum pt of all Particles with pt > %1f GeV/c within radii %.2f and %.2f", *ptiter,rlim[irad],rlim[irad+1]), 100, 0., 500., 500., 0. , 500);
+       fPtHistoMapAll->Insert(*ptiter, rlim[irad], rlim[irad+1], result);
+       fOutput->Add(result);
+
+       result = new TH2D(Form("hSumPtChargedPt%03dRadmin%03dRadmax%03d", int((*ptiter) *10.), int(rlim[irad]*100), int(rlim[irad+1]*100)),
+           Form("Sum pt of charged Particles with pt > %1f GeV/c within radii %.2f and %.2f", *ptiter,rlim[irad],rlim[irad+1]), 100, 0., 500., 500., 0. , 500);
+       fPtHistoMapCharged->Insert(*ptiter, rlim[irad], rlim[irad+1], result);
+       fOutput->Add(result);
+     }
+   }
+}
+
+Bool_t SelectorJetStructure::Process(Long64_t entry)
+{
+   // The Process() function is called for each entry in the tree (or possibly
+   // keyed object in the case of PROOF) to be processed. The entry argument
+   // specifies which entry in the currently loaded tree is to be processed.
+   // It can be passed to either SelectorJetStructure::GetEntry() or TBranch::GetEntry()
+   // to read either all or the required parts of the data. When processing
+   // keyed objects with PROOF, the object is already loaded and is available
+   // via the fObject pointer.
+   //
+   // This function should contain the "body" of the analysis. It can contain
+   // simple or elaborate selection criteria, run algorithms on the data
+   // of the event and typically fill histograms.
+   //
+   // The processing can be stopped by calling Abort().
+   //
+   // Use fStatus to set the return value of TTree::Process().
+   //
+   // The return value is currently not used.
+
+   GetEntry(entry);
+
+   double pttrackmin[] = {0., 0.5, 1., 2., 5., 10., 20., 50.};
+   double rlim[] = {0, 0.05, 0.1, 0.2, 0.3, 0.4};
+
+   double weight = JetEvent->GetCrossSection()/static_cast<double>(JetEvent->GetNumberOfTrials());
+
+   for(double *ptiter = pttrackmin; ptiter < pttrackmin + sizeof(pttrackmin)/sizeof(double); ++ptiter){
+     for(int irad = 0; irad < 5; irad++){
+       TIter jetIter(JetEvent->GetListOfJets());
+       AliReducedJetInfo *myjet(NULL);
+       while((myjet = dynamic_cast<AliReducedJetInfo *>(jetIter))){
+         InspectJet(myjet, *ptiter, rlim[irad], rlim[irad+1], true, weight);
+         InspectJet(myjet, *ptiter, rlim[irad], rlim[irad+1], false, weight);
+       }
+     }
+   }
+   return kTRUE;
+}
+
+void SelectorJetStructure::SlaveTerminate()
+{
+   // The SlaveTerminate() function is called after all entries or objects
+   // have been processed. When running with PROOF SlaveTerminate() is called
+   // on each slave server.
+
+}
+
+void SelectorJetStructure::Terminate()
+{
+   // The Terminate() function is the last function to be called during
+   // a query. It always runs on the client, it can be used to present
+   // the results graphically or save the results to file.
+
+  TList *lcontrib = new TList;
+  TList *lsumpt = new TList;
+
+  TList *lcall = new TList; lcall->SetName("all");
+  TList *lccharged = new TList; lccharged->SetName("charged");
+  lcontrib->Add(lcall); lcontrib->Add(lccharged);
+  TList *lsall = new TList; lsall->SetName("all");
+  TList *lscharged = new TList; lscharged->SetName("charged");
+  lsumpt->Add(lsall); lsumpt->Add(lscharged);
+
+
+  TIter histIter(fOutput);
+  TObject *histo;
+  while((histo = histIter())){
+    TString histname(histo->GetName());
+    if(histname.Contains("hContrib")){
+      if(histname.Contains("Charged")) lccharged->Add(histo);
+      else lcall->Add(histo);
+    }
+    else if(histname.Contains("hContrib")){
+      if(histname.Contains("Charged")) lscharged->Add(histo);
+      else lsall->Add(histo);
+    }
+  }
+
+  TFile *jetstructure = new TFile("jetstructure.root", "RECREATE");
+  jetstructure->cd();
+  lcontrib->Write("ncontrib", TObject::kSingleKey);
+  lsumpt->Write("sumpt", TObject::kSingleKey);
+  delete jetstructure;
+}
+
+void SelectorJetStructure::InspectJet(AliReducedJetInfo* jet, double ptmin, double radmin, double radmax, bool charged, double weight) {
+  int nconst = 0;
+  double sumpt = 0.;
+  TIter constIter(jet->GetListOfConstituents());
+  AliReducedJetConstituent *jconst = NULL;
+
+  TLorentzVector jetvec;
+  jet->FillLorentzVector(jetvec);
+  while((jconst = dynamic_cast<AliReducedJetConstituent *>(constIter()))){
+    TLorentzVector ptvec;
+    jconst->FillLorentzVector(ptvec);
+    if(TMath::Abs(ptvec.Eta()) > 0.8) continue;
+    if(TMath::Abs(ptvec.Pt()) < ptmin) continue;
+    if(charged){
+      if(!jconst->GetPDGParticle()->Charge()) continue;
+    }
+    double r = TMath::Abs(jetvec.DeltaR(ptvec));
+    if(r < radmin || r > radmax) continue;
+    nconst++;
+    sumpt += TMath::Abs(ptvec.Pt());
+  }
+  TH2 *htofill(NULL), htofillpt(NULL);
+  if(charged){
+    htofill = fContribHistoMapCharged->FindHistogram(ptmin, radmin, radmax);
+    htofillpt = fPtHistoMapCharged->FindHistogram(ptmin, radmin, radmax);
+  } else {
+    htofill = fContribHistoMapAll->FindHistogram(ptmin, radmin, radmax);
+    htofillpt = fPtHistoMapAll->FindHistogram(ptmin, radmin, radmax);
+  }
+  if(htofill) htofill->Fill(TMath::Abs(jetvec.Pt()), nconst, weight);
+  if(htofillpt) htofill->Fill(TMath::Abs(jetvec.Pt()), sumpt, weight);
+}
---
+++ source/SelectorJetStructure.h
@@ -0,0 +1,151 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Fri Jan 30 06:15:28 2015 by ROOT version 5.34/08
+// from TTree JetEvent/A tree with jet information
+// found on file: root_archive.zip#AnalysisResults.root
+//////////////////////////////////////////////////////////
+
+#ifndef SelectorJetStructure_h
+#define SelectorJetStructure_h
+
+#include <vector>
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TSelector.h>
+
+// Header file for the classes stored in the TTree if any.
+#include "AliReducedJetEvent.h"
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class TH2;
+
+//class AliReducedJetInfo;
+
+
+class HistoList{
+public:
+  HistoList() : fHistograms() {}
+  ~HistoList() {}
+
+  void Insert(double ptmin, double rmin, double rmax, TH2 *histo){
+    std::vector<HistoContent>::iterator it = FindBin(ptmin, rmin, rmax);
+    if(it != fHistograms.end()) it->SetHisto(histo);
+    else
+      fHistograms.push_back(HistoContent(ptmin, rmin, rmax, histo));
+  }
+  TH2 * FindHistogram(double ptmin, double rmin, double rmax){
+    TH2 *result = NULL;
+    std::vector<HistoContent>::iterator it = FindBin(ptmin, rmin, rmax);
+    if(it != fHistograms.end()) result = it->GetHisto();
+    return result;
+  }
+private:
+  class HistoContent{
+  public:
+    HistoContent(double ptmin, double rmin, double rmax, TH2 *histo):
+      fPtMin(ptmin),
+      fRMin(rmin),
+      fRMax(rmax),
+      fHistogram(histo)
+    {}
+    ~HistoContent() {}
+
+    double IsEqual(double ptmin, double rmin, double rmax) const {
+      return (fPtMin == ptmin && fRMin == rmin && fRMax == rmax);
+    }
+    TH2 *GetHisto() const { return fHistogram; }
+    void SetHisto(TH2 *hist) { fHistogram = hist; }
+
+  private:
+    double        fPtMin;
+    double        fRMin;
+    double        fRMax;
+    TH2           *fHistogram;
+  };
+  std::vector<HistoContent>::iterator FindBin(double ptmin, double rmin, double rmax){
+    std::vector<HistoContent>::iterator result = fHistograms.end();
+    for(std::vector<HistoContent>::iterator it = fHistograms.begin(); it != fHistograms.end(); ++it){
+      if(it->IsEqual(ptmin, rmin, rmax)){
+        result = it;
+        break;
+      }
+    }
+    return result;
+  }
+  std::vector<HistoContent>     fHistograms;
+};
+
+class SelectorJetStructure : public TSelector {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+
+   // Declaration of leaf types
+   HighPtTracks::AliReducedJetEvent *JetEvent;
+
+   HistoList     *fContribHistoMapAll;
+   HistoList     *fContribHistoMapCharged;
+   HistoList     *fPtHistoMapAll;
+   HistoList     *fPtHistoMapCharged;
+
+   // List of branches
+   TBranch        *b_JetEvent;   //!
+
+   SelectorJetStructure(TTree * /*tree*/ =0) : fChain(0), fContribHistoMapAll(NULL), fContribHistoMapCharged(NULL), fPtHistoMapAll(NULL), fPtHistoMapCharged(NULL) { }
+   virtual ~SelectorJetStructure() { }
+   virtual Int_t   Version() const { return 2; }
+   virtual void    Begin(TTree *tree);
+   virtual void    SlaveBegin(TTree *tree);
+   virtual void    Init(TTree *tree);
+   virtual Bool_t  Notify();
+   virtual Bool_t  Process(Long64_t entry);
+   virtual Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
+   virtual void    SetOption(const char *option) { fOption = option; }
+   virtual void    SetObject(TObject *obj) { fObject = obj; }
+   virtual void    SetInputList(TList *input) { fInput = input; }
+   virtual TList  *GetOutputList() const { return fOutput; }
+   virtual void    SlaveTerminate();
+   virtual void    Terminate();
+
+   void InspectJet(HighPtTracks::AliReducedJetInfo *jet, double ptmin, double radmin, double radmax, bool charged, double weight);
+
+   ClassDef(SelectorJetStructure,0);
+};
+
+#endif
+
+#ifdef SelectorJetStructure_cxx
+void SelectorJetStructure::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   JetEvent = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("JetEvent", &JetEvent, &b_JetEvent);
+}
+
+Bool_t SelectorJetStructure::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+#endif // #ifdef SelectorJetStructure_cxx
