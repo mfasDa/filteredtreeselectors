d9546b7860ab44b957cf9de39731812801b0a2e7
mfasel <mfasel@lbl.gov>
2015-02-02 19:17:29 +0100

    Test files in pieces of 100 in one shot

--- steer/submitTest.sh
+++ steer/submitTest.sh
@@ -8,8 +8,19 @@ sourcedirin=`dirname $0`
 sourcedir=`readlink -f $sourcedirin`
 
 files=($(find $inputdir -name root_archive.zip))
+nfiles=0
+ntest=0
+testbase=$(readlink -f $GSCRATCH/filetest/)
+testdir=$testbase/job$ntest
 for f in ${files[@]}; do
 	myfile=$(printf "%s#%s" $f $rootfilename)
-	cmd=$(printf "qsub -l projectio=1,gscratchio=1 -o %s/filetest.log -e %s/filetesterr.log %s/testFile.sh %s %s %s %s %s" $inputdir $inputdir $sourcedir $sourcedir $myfile $treename $inputdir `readlink -f $GSCRATCH`)
-	eval $cmd
+	echo $myfile >> $testdir/files.txt
+	let "nfiles++
+	if [ $nfiles -ge 100 ]; then 
+		qcmd=$(printf "qsub -l projectio=1,gscratchio=1 -o %s/filetest.log -e %s/filetesterr.log %s/testFile.sh %s %s %s " $inputdir $inputdir $sourcedir $sourcedir $testdir $treename)
+		eval $cmd
+		let "ntest++"
+		testdir=$testbase/job$ntest
+		nfiles=0
+	fi
 done
--- steer/testFile.sh
+++ steer/testFile.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
 sourcedir=$1
-filetotest=$2
+workdir=$2
 treetotest=$3
-outputdir=$4
-gscratch=$5
 
 . /usr/share/Modules/init/bash
 module use /cvmfs/alice.cern.ch/x86_64-2.6-gnu-4.1.2/Modules/modulefiles
@@ -13,36 +11,26 @@ module load cgal/v4.4
 module load fastjet/v3.0.6_1.012
 module load AliEn/v2-19-217
 
-WD=$gscratch/filetest/$JOB_ID
-if [ ! -d $WD ]; then mkdir -p $WD; fi
-echo Workdir $WD
 
-cmd=$(printf "root -b -q %s/LoadLibs.C \'%s/testfile.C(\"%s\",\"%s\")\' &> %s/test.log" $sourcedir $sourcedir $filetotest $treetotest $WD) 
-eval $cmd
-status=$?
-
-# analyse logfile
-logbad=0
 while read line; do
-	badalloctest=$(echo $line | grep "std::bad_alloc")
-	if [ "x$badalloctest" != "x" ]; then 
-		# bad alloc thrown
-		logbad=1
-		break
-	fi
-done < $WD/test.log
+	cmd=$(printf "root -b -q %s/LoadLibs.C \'%s/testfile.C(\"%s\",\"%s\")\' &> %s/test.log" $sourcedir $sourcedir $line $treetotest $workdir) 
+	eval $cmd
+	status=$?
 
-while [ -f $outputdir/lock ]; do
-	#directory locked
-	sleep 5;
-done
-# set lock
-touch $outputdir/lock
-if [ $status -gt 1 -o $logbad -ne 0 ]; then
-	echo $filetotest >> $outputdir/badfiles.txt
-else
-echo $filetotest >> $outputdir/goodfiles.txt
-fi
-# release lock
-rm $outputdir/lock
-rm -rf $WD
+	# analyse logfile
+	logbad=0
+	while read line; do
+		badalloctest=$(echo $line | grep "std::bad_alloc")
+		if [ "x$badalloctest" != "x" ]; then 
+			# bad alloc thrown
+			logbad=1
+			break
+		fi
+	done < $workdir/test.log
+
+	if [ $status -gt 1 -o $logbad -ne 0 ]; then
+		echo $filetotest >> $workdir/badfiles.txt
+	else
+		echo $filetotest >> $workdir/goodfiles.txt
+	fi
+done < $workdir/files.txt
