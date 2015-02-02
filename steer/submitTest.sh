5c5988e9e9e9e6b27a90fbc16c97a0d00aa5d1ea
mfasel <mfasel@lbl.gov>
2015-02-02 19:27:37 +0100

    handing of log files

--- steer/submitTest.sh
+++ steer/submitTest.sh
@@ -3,6 +3,7 @@
 inputdir=`readlink -f $1`
 rootfilename=$2
 treename=$3
+outputbase=$4
 
 sourcedirin=`dirname $0`
 sourcedir=`readlink -f $sourcedirin`
@@ -10,7 +11,8 @@ sourcedir=`readlink -f $sourcedirin`
 files=($(find $inputdir -name root_archive.zip))
 nfiles=0
 ntest=0
-testbase=$(readlink -f $GSCRATCH/filetest/)
+if [ ! -d $outputbase ]; then mkdir -p $outputbase; fi
+testbase=$(readlink -f $outputbase)
 testdir=$testbase/job$ntest
 mkdir -p $testdir
 for f in ${files[@]}; do
@@ -18,7 +20,7 @@ for f in ${files[@]}; do
 	echo $myfile >> $testdir/files.txt
 	let "nfiles++"
 	if [ $nfiles -ge 100 ]; then 
-		qcmd=$(printf "qsub -l projectio=1,gscratchio=1 -o %s/filetest.log -e %s/filetesterr.log %s/testFile.sh %s %s %s" $inputdir $inputdir $sourcedir $sourcedir $testdir $treename)
+		qcmd=$(printf "qsub -l projectio=1,gscratchio=1 -o %s/filetest.log -e %s/filetesterr.log %s/testFile.sh %s %s %s" $testbase $testbase $sourcedir $sourcedir $testdir $treename)
 		eval $qcmd
 		let "ntest++"
 		testdir=$testbase/job$ntest
@@ -28,6 +30,6 @@ for f in ${files[@]}; do
 done
 if [ $nfiles -gt 0 ]; then
 	#submit last bunch
-	qcmd=$(printf "qsub -l projectio=1,gscratchio=1 -o %s/filetest.log -e %s/filetesterr.log %s/testFile.sh %s %s %s" $inputdir $inputdir $sourcedir $sourcedir $testdir $treename)
+	qcmd=$(printf "qsub -l projectio=1,gscratchio=1 -o %s/filetest.log -e %s/filetesterr.log %s/testFile.sh %s %s %s" $testbase $testbase $sourcedir $sourcedir $testdir $treename)
 	eval $qcmd
 fi
