3153b5ca3ce46c94ee0d8d650a1df86f6d318921
mfasel <mfasel@lbl.gov>
2015-02-02 18:07:45 +0100

    Bugfix + simplify

--- steer/testfile.C
+++ steer/testfile.C
@@ -5,27 +5,7 @@
 void testfile(const char *filename, const char *treename){
   TFile *in = TFile::Open(filename);
 
-  TTree *totest(0x0);
-  TString tnstring(treename);
-  if(tnstring.Contains("/")){
-    TObjArray *tokens = tnstring.Tokenize("/");
-    int ntok(0);
-    TIter tokIter(tokens);
-    TObjString *mystr(0x0);
-    while((mystr = dynamic_cast<TObjString *>(tokIter))){
-      if(ntok < tokens->GetEntries()-1){
-        if(ntok == 0)
-          gFile->cd(mystr->String().Data());
-        else
-          gDirectory->cd(mystr->String().Data());
-      } else {
-        totest = gDirectory->Get(mystr->String().Data());
-      }
-      ntok++;
-    }
-  } else {
-    totest = (TTree *)in->Get(treename);
-  }
+  TTree *totest = (TTree *)in->Get(treename);
 
   // Loop over all entries, see whether it crashes
   HighPtTracks::AliReducedJetEvent *myevent;
