af6931f1401a98de17b4ab1b324a5ff9728ad4ad
mfasel <mfasel@lbl.gov>
2015-02-02 19:22:01 +0100

    fix + submit last bunch

--- steer/submitTest.sh
+++ steer/submitTest.sh
@@ -12,6 +12,7 @@ nfiles=0
 ntest=0
 testbase=$(readlink -f $GSCRATCH/filetest/)
 testdir=$testbase/job$ntest
+mkdir -p $testdir
 for f in ${files[@]}; do
 	myfile=$(printf "%s#%s" $f $rootfilename)
 	echo $myfile >> $testdir/files.txt
@@ -21,6 +22,12 @@ for f in ${files[@]}; do
 		eval $cmd
 		let "ntest++"
 		testdir=$testbase/job$ntest
+		mkdir -p $testdir
 		nfiles=0
 	fi
 done
+if [ $nfiles -gt 0 ]; then
+	#submit last bunch
+	qcmd=$(printf "qsub -l projectio=1,gscratchio=1 -o %s/filetest.log -e %s/filetesterr.log %s/testFile.sh %s %s %s" $inputdir $inputdir $sourcedir $sourcedir $testdir $treename)
+	eval $cmd
+fi
