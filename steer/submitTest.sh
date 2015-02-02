12c5554628ec4fe27d59ecbf23bab252cdfc8637
mfasel <mfasel@lbl.gov>
2015-02-02 18:13:40 +0100

    small fix

--- steer/submitTest.sh
+++ steer/submitTest.sh
@@ -7,7 +7,7 @@ treename=$3
 sourcedirin=`dirname $0`
 sourcedir=`readlink -f $sourcedirin`
 
-files=($(find $inputdir --name root_archive.zip))
+files=($(find $inputdir -name root_archive.zip))
 for f in ${files[@]}; do
 	myfile=$(printf "%s#%s" $f $rootfilename)
 	cmd=$(printf "qsub -o %s/filetest.log -e %s/filetesterr.log %s/testFile.sh %s %s %s %s" $inputdir $inputdir $sourcedir $sourcedir $myfile $treename $inputdir)
