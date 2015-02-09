8219dd46dd9f80265fae18df7f0a72cf7842cec9
mfasel <mfasel@lbl.gov>
2015-02-09 09:33:27 +0100

    fix handling of home directory

--- steer/copy_merged.py
+++ steer/copy_merged.py
@@ -7,10 +7,16 @@ def main():
     outputpath = sys.argv[2]
     rootfile = sys.argv[3]
     
-    directories = os.listdir(inputpath)
+    if "~" in inputhpath:
+        inputpath = inputpath.replace("~", os.path.expanduser("~"))
+    if "~" in outputpath:
+        outputpath = outputpath.replace("~", os.path.expanduser("~"))
+    
+    directories = os.listdir(os.readlink(inputpath))
     for od in directories:
         outputdir = os.path.join(ouputpath, od)
-        os.makedirs(outputdir)
+        if not os.path.exists(outputdir):
+            os.makedirs(outputdir, 0755)
         shutil.copy(os.path.join(inputpath, dir, rootfile), outputdir)
         
 if __name__ == "__main__":
