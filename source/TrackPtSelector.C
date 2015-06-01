36ad57c3421e5ec419d07da0f515dd9f1c85352c
mfasel <markus.fasel@cern.ch>
2015-06-01 16:14:11 +0200

    Update track pt selector

--- source/TrackPtSelector.C
+++ source/TrackPtSelector.C
@@ -23,6 +23,8 @@
 // Root > T->Process("TrackPtSelector.C+")
 //
 
+#include <cstring>
+#include <iostream>
 #include <map>
 #include <string>
 #include <vector>
@@ -34,6 +36,9 @@
 
 #include "TrackPtSelector.h"
 #include "THistManager.h"
+#include "AliReducedPatchInfo.h"
+#include "AliReducedPatchContainer.h"
+#include "AliReducedEmcalCluster.h"
 #include "AliReducedReconstructedTrack.h"
 
 
@@ -62,8 +67,13 @@ void TrackPtSelector::SlaveBegin(TTree * /*tree*/)
 
    const int kNtriggers = 5;
    std::string triggernames[kNtriggers] = {"MinBias", "EMCGHigh", "EMCGLow", "EMCJHigh", "EMCJLow"};
-   for(std::string *trigger = triggernames; trigger < triggernames + kNtriggers; ++trigger)
+   for(std::string *trigger = triggernames; trigger < triggernames + kNtriggers; ++trigger){
+   	std::cout << "Creating histogram for trigger " << *trigger << std::endl;
+   	fHistos->CreateTH1(Form("hEnergy%s", trigger->c_str()), Form("Cluster energy for trigger class %s", trigger->c_str()), ptBinning);
    	fHistos->CreateTH1(Form("hPt%s", trigger->c_str()), Form("Track pt for trigger class %s", trigger->c_str()), ptBinning);
+   	if(strcmp(trigger->c_str(), "MinBias"))
+   		fHistos->CreateTH1(Form("hPatchAmplitude%s", trigger->c_str()), Form("Patch amplitude for %s patches", trigger->c_str()), 1000, 0., 1000.);
+   }
 
    fOutput->Add(fHistos);
 }
@@ -104,7 +114,33 @@ Bool_t TrackPtSelector::Process(Long64_t entry)
 	if(ReducedEvent->IsGammaHighFromString()) triggerstrings.push_back("EMCGHigh");
 	if(ReducedEvent->IsGammaLowFromString()) triggerstrings.push_back("EMCGLow");
 
-	for(TIter trackIter = TIter(ReducedEvent->GetTrackContainer()); trackIter != TIter::End(); ++trackIter){
+	HighPtTracks::AliReducedPatchContainer *triggerpatches = ReducedEvent->GetPatchContainer();
+	for(TIter patchIter = TIter(triggerpatches->GetTriggerPatches(kFALSE, HighPtTracks::AliReducedPatchContainer::kEMCJetHigh)).Begin(); patchIter != TIter::End(); ++patchIter){
+		HighPtTracks::AliReducedPatchInfo *patch = dynamic_cast<HighPtTracks::AliReducedPatchInfo *>(*patchIter);
+		if(patch) fHistos->FillTH1("hPatchAmplitudeEMCJHigh", patch->GetAmplitude());
+	}
+	for(TIter patchIter = TIter(triggerpatches->GetTriggerPatches(kFALSE, HighPtTracks::AliReducedPatchContainer::kEMCJetLow)).Begin(); patchIter != TIter::End(); ++patchIter){
+		HighPtTracks::AliReducedPatchInfo *patch = dynamic_cast<HighPtTracks::AliReducedPatchInfo *>(*patchIter);
+		if(patch) fHistos->FillTH1("hPatchAmplitudeEMCJLow", patch->GetAmplitude());
+	}
+	for(TIter patchIter = TIter(triggerpatches->GetTriggerPatches(kFALSE, HighPtTracks::AliReducedPatchContainer::kEMCGammaHigh)).Begin(); patchIter != TIter::End(); ++patchIter){
+		HighPtTracks::AliReducedPatchInfo *patch = dynamic_cast<HighPtTracks::AliReducedPatchInfo *>(*patchIter);
+		if(patch) fHistos->FillTH1("hPatchAmplitudeEMCGHigh", patch->GetAmplitude());
+	}
+	for(TIter patchIter = TIter(triggerpatches->GetTriggerPatches(kFALSE, HighPtTracks::AliReducedPatchContainer::kEMCGammaLow)).Begin(); patchIter != TIter::End(); ++patchIter){
+		HighPtTracks::AliReducedPatchInfo *patch = dynamic_cast<HighPtTracks::AliReducedPatchInfo *>(*patchIter);
+		if(patch) fHistos->FillTH1("hPatchAmplitudeEMCGLow", patch->GetAmplitude());
+	}
+
+	//std::cout << "Number of clusters: " << ReducedEvent->GetClusterContainer()->GetEntries() << std::endl;
+	for(TIter clustIter = TIter(ReducedEvent->GetClusterContainer()).Begin(); clustIter != TIter::End(); ++clustIter){
+		HighPtTracks::AliReducedEmcalCluster *clust = dynamic_cast<HighPtTracks::AliReducedEmcalCluster *>(*clustIter);
+		for(std::vector<std::string>::iterator trgit = triggerstrings.begin(); trgit != triggerstrings.end(); ++trgit)
+			fHistos->FillTH1(Form("hEnergy%s", trgit->c_str()), clust->GetClusterEnergy());
+	}
+
+	//std::cout << "Number of clusters: " << ReducedEvent->GetClusterContainer()->GetEntries() << std::endl;
+	for(TIter trackIter = TIter(ReducedEvent->GetTrackContainer()).Begin(); trackIter != TIter::End(); ++trackIter){
 		HighPtTracks::AliReducedReconstructedTrack *track = dynamic_cast<HighPtTracks::AliReducedReconstructedTrack *>(*trackIter);
 		if(TMath::Abs(track->Eta()) > 0.8) continue;
 		if(track->TestTrackCuts(kDefaultCuts)) continue;
@@ -127,12 +163,19 @@ void TrackPtSelector::Terminate()
    // The Terminate() function is the last function to be called during
    // a query. It always runs on the client, it can be used to present
    // the results graphically or save the results to file.
-
-	fHistos = fOutput->FindObject("TrackPtHistos");
+	std::cout << "In terminate" << std::endl;
+	fHistos = dynamic_cast<THistManager *>(fOutput->FindObject("TrackPtHistos"));
+	if(!fHistos){
+		std::cout << "Histogram manager not found" << std::endl;
+	}
 
 	TFile *result = new TFile("trackpt.root", "RECREATE");
-	for(TIter histIter = TIter(fHistos->GetListOfHistograms()); histIter != TIter::End(); ++ histIter)
+	result->cd();
+	std::cout << fHistos->GetListOfHistograms()->GetEntries() << " histograms found" << std::endl;
+	for(TIter histIter = TIter(fHistos->GetListOfHistograms()).Begin(); histIter != TIter::End(); ++histIter){
+		std::cout << "Writing histogram " << (*histIter)->GetName() << std::endl;
 		(*histIter)->Write();
+	}
 	result->Close();
 	delete result;
 }
